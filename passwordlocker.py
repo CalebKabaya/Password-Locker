70fe391fc435e1a5522fed985d6daa81a23cfcf5
calebkabaya <calebkabaya@gmail.com>
2022-04-23 13:48:57 +0300

    ft-test: created the test methods for setup and test init

--- passwordlocker.py
+++ passwordlocker.py
@@ -1,4 +1,23 @@
+
+
 class User :
-    pass
+    """"
+     my class which i will use to craeat new instances
+     """
+    user_list = []
+
+    def __init__(self,name,password):
+     self.name = name
+     self.password  = password
+
+# def save_user(self):
+#    """
+#     a method to save a new user to the list
+#     """
+
+#    User.user_list.append(self)
+
+
+
 class Credentials:
     pass
--- passwordlocker_test.py
+++ passwordlocker_test.py
@@ -1,3 +1,28 @@
+from unicodedata import name
 import unittest #importing the unittest module
 from passwordlocker import User
 from passwordlocker import Credentials
+
+class TestUser(unittest.TestCase):
+    """
+    Test class that defines test cases for the contact class behaviours.
+
+    Args:
+        unittest.TestCase: TestCase class that helps in creating test cases
+    """
+    def setUp(self):
+        '''
+        Set up method to run before each test cases.
+        '''
+        self.new_user = User('CalebKabaya', 'Mbuguack')
+
+    
+    def test_init(self):
+        '''
+        test_init test case to test if the object is initialized properly
+        '''
+        self.assertEqual(self.new_user.name,'CalebKabaya')
+        self.assertEqual(self.new_user.password,'Mbuguack')
+
+if __name__ == '__main__':
+    unittest.main()        
